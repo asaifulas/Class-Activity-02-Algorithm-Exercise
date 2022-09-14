075a005ff1b67440e2f6ced62fbb38ebcd7122dc
Ahmad Saifullah Arifin <asaifulas@gmail.com>
2022-09-14 21:53:56 +0800

    add file

---
+++ 09_factorial_number.js
@@ -0,0 +1,3 @@
+//Write a function to print the factorial of number n. 
+//If n = 5, then factorial of 5 is 5 * 4 * 3 * 2 * 1. 
+//If n = 10, the factorial of 10 is 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
